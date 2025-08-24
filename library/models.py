1bbcea935021843652d56c6530b94b8abef832b4
Rusich <vasenin@verqor.com>
2025-08-24 19:06:42 +0300

    added book model

--- library/models.py
+++ library/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from tinymce.models import HTMLField
 
-# Create your models here.
+from users.models import Profile
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=250, unique=True)
+    author = models.ForeignKey(Profile, on_delete=models.DO_NOTHING)
+    description = HTMLField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
