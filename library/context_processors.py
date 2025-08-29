9be5ba20772f49e479d0c319cdfa5a2632763cb8
Rusich <vasenin@verqor.com>
2025-08-29 23:16:35 +0300

    added my books

--- library/context_processors.py
+++ library/context_processors.py
@@ -5,5 +5,6 @@ def menu_data(request):
     return {
         'authors_count': Profile.objects.filter(isAuthor=True).count(),
         'friends_count': request.user.profile.friends.count() if request.user.is_authenticated and hasattr(request.user, 'profile') else 0,
+        'requests_count': request.user.profile.friends_request_to_me.count() if request.user.is_authenticated and hasattr(request.user, 'profile') else 0,
         'books_count': Book.objects.all().count(),
     }
