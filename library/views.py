55a25627f8b466cc5d542229901bb344e0c9faa6
Rusich <vasenin@verqor.com>
2025-09-03 22:21:23 +0300

    added book create part

--- library/views.py
+++ library/views.py
@@ -111,6 +111,12 @@ class BookDetailsView(DetailView):
     template_name = 'library/book-details.html'
     context_object_name = 'book'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        return context
+#         author_is_friend
+
 
 class AuthorDetailView(DetailView):
     model = Profile
